1883090163bf20beb85bbbcf7e3bfc54b2c6b987
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-23 15:51:10 +0100

    No code changes made; skipping commit.

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -1,16 +1,6 @@
 // components/Layout.js
-import React, { ReactNode } from 'react';
+import React, { ReactNode } from "react";
 
 export default function Layout({ children }: { children: ReactNode }) {
-  return <div 
-  
-  
-
-
-  
-
-
-  
-
-  className='bg-white'>{children}</div>;
+  return <div className="bg-white">{children}</div>;
 }
