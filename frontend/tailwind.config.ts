5ff1416332b1493859e9c64a42cb6eb24ee26028
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-23 11:07:52 +0100

    Enhance Tailwind CSS configuration with custom colors and font family

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,16 +1,29 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 export default {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
+      fontFamily: {
+        heebo: ['var(--font-heebo)', 'sans-serif']
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: '#21243D',         // Dark Blue Text
+        secondary: '#8695A4',       // Muted Text
+        button: {
+          primary: '#FF6464',       // Red Button
+        },
+        background: {
+          primary: '#FFFFFF',       // White
+          secondary: '#EDF7FA',     // Light Blue Background
+        },
+        link: {
+          primary: '#00A8CC',       // Blue Links
+        },
       },
     },
   },
