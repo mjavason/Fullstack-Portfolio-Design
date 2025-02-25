7796cb53c8e49e54dae0d4f8ebde05a252907134
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-25 10:30:36 +0100

    Update button padding and profile image width for improved layout consistency

--- frontend/src/components/sections/hero/profile-image.tsx
+++ frontend/src/components/sections/hero/profile-image.tsx
@@ -4,7 +4,7 @@ function ProfileImage() {
   return (
     <div className="order-1 lg:order-2 flex flex-col items-center">
       <Image
-        className="object-contain w-2/3 lg:w-auto m-auto rounded-full lg:flex-1 shadow-[-5px_13px_5px_#EDF7FA]"
+        className="object-contain w-2/3 lg:w-full m-auto rounded-full lg:flex-1 shadow-[-5px_13px_5px_#EDF7FA]"
         src="/images/Ellipse 1.png"
         alt="Picture of John"
         isZoomed
