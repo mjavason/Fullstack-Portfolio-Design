b920c24c41ae283f1c2a394217a515727414735f
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-25 10:57:47 +0100

    Refactor profile description component: update heading structure for improved semantics and layout

--- frontend/src/components/sections/hero/profile-description.tsx
+++ frontend/src/components/sections/hero/profile-description.tsx
@@ -3,10 +3,9 @@ import { Button } from '@heroui/react';
 function ProfileDescription() {
   return (
     <div className="order-2 lg:order-1 flex-1 flex flex-col gap-10 w-full lg:w-2/3 text-[#21243D]">
-      <h1 className="px-3 lg:px-0 text-4xl lg:text-5xl font-bold">
-        Hi, I am John.
-        <br />
-        Creative Technologist
+      <h1 className="flex flex-col gap-3 px-3 lg:px-0 text-4xl lg:text-5xl font-bold">
+        <p>Hi, I am John.</p>
+        <p>Creative Technologist</p>
       </h1>
       <p className="text-xl">
         Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
