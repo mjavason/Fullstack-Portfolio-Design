18cc1804d144383d9fe2cfc6720b84e40783f3b3
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-25 11:17:50 +0100

    Refactor FeaturedWorks component: enhance image layout and styling for better responsiveness and visual appeal

--- frontend/src/components/sections/featured-works/works.tsx
+++ frontend/src/components/sections/featured-works/works.tsx
@@ -8,14 +8,16 @@ async function FeaturedWorks() {
   const renderedWorks = works.map((work, index) => {
     return (
       <Link key={index} href={paths.workDetails(index.toString())}>
-        <div className="flex flex-col md:flex-row gap-5 md:gap-0 hover:shadow-md border-b-2 pb-3">
-          <Image
-            className="object-contain w-[100%] md:w-fit h-[100%]"
-            src={work.image}
-            alt={work.alt}
-            isZoomed
-            removeWrapper
-          />
+        <div className="flex flex-col items-stretch md:flex-row gap-5 md:gap-0 hover:shadow-md border-b-2 pb-3">
+          <div className="overflow-hidden w-2/5">
+            <Image
+              className="object-cover w-full"
+              src={work.image}
+              alt={work.alt}
+              isZoomed
+              removeWrapper
+            />
+          </div>
           <div className="flex flex-col gap-5 md:gap-0 justify-between cursor-pointer bg-white px-0 md:px-5">
             <h3 className="text-3xl font-bold">{work.title}</h3>
             <div className="my-3 text-lg flex items-center gap-5">
