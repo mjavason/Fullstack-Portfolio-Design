161503b744d7a9c8ae485b314df587f1444fd501
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-02-25 14:52:57 +0100

    Refactor MobileList component: update structure and styling for improved navigation experience

--- frontend/src/components/navigation/mobile-list.tsx
+++ frontend/src/components/navigation/mobile-list.tsx
@@ -5,23 +5,27 @@ function MobileList() {
   return (
     <>
       <input type="checkbox" id="nav-toggle" className="hidden peer" />
-      <ul className="fixed top-0 left-0 h-screen w-1/3 bg-gray-600 shadow-lg transform -translate-x-full peer-checked:translate-x-0 transition-transform duration-300 ease-in-out flex flex-col justify-center items-start p-5 space-y-4 font-bold sm:hidden z-50">
-        <li className="px-4 py-2 rounded transition-colors duration-300 cursor-pointer">
-          <Link className="text-white hover:text-button-primary" href={paths.work}>
-            Work
-          </Link>
-        </li>
-        <li className="px-4 py-2 rounded transition-colors duration-300 cursor-pointer">
-          <Link className="text-white hover:text-button-primary" href={paths.blog}>
-            Blog
-          </Link>
-        </li>
-        <li className="px-4 py-2 rounded transition-colors duration-300 cursor-pointer">
-          <Link className="text-white hover:text-button-primary" href={paths.home}>
-            Contact
-          </Link>
-        </li>
-      </ul>
+      <div className="fixed top-0 left-0 h-screen w-screen bg-transparent shadow-lg transform -translate-x-full peer-checked:translate-x-0 transition-transform duration-300 ease-in-out flex font-bold sm:hidden z-50">
+        <ul className="h-full w-1/3 bg-gray-600 shadow-lg flex flex-col justify-center items-start p-5 space-y-4">
+          <li className="px-4 py-2 rounded transition-colors duration-300 cursor-pointer">
+            <Link className="text-white hover:text-button-primary" href={paths.work}>
+              Work
+            </Link>
+          </li>
+          <li className="px-4 py-2 rounded transition-colors duration-300 cursor-pointer">
+            <Link className="text-white hover:text-button-primary" href={paths.blog}>
+              Blog
+            </Link>
+          </li>
+          <li className="px-4 py-2 rounded transition-colors duration-300 cursor-pointer">
+            <Link className="text-white hover:text-button-primary" href={paths.home}>
+              Contact
+            </Link>
+          </li>
+        </ul>
+
+        <label htmlFor="nav-toggle" className="flex-1 h-full bg-transparent"></label>
+      </div>
     </>
   );
 }
