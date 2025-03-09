00ba083771dde43c269ef854111acd32cd5c8b6e
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-03-09 17:23:54 +0100

    Update BASE_URL constant to use NEXT_PUBLIC_BASE_API_URL for environment configuration

--- frontend/src/config/constants/index.ts
+++ frontend/src/config/constants/index.ts
@@ -3,7 +3,7 @@ dotenv.config();
 
 // "https://fullstack-portfolio-backend-yyhy.onrender.com/api/v1";
 // "http://localhost:5000/api/v1"
-export const BASE_URL = process.env.BASE_API_URL || 'http://localhost:5000/api/v1';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_API_URL || 'http://localhost:5000/api/v1';
 
 export const lightBlue = '#A5D0FF';
 export const darkBlue = '#4CA2FE';
