fc4e697fb80a6a7dd3dbdf002dea96c1b80599cf
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-03-16 22:19:08 +0100

    Add responsive navbar to admin side

--- frontend/src/components/admin/navigation/index.tsx
+++ frontend/src/components/admin/navigation/index.tsx
@@ -5,11 +5,27 @@ import { usePathname, useRouter } from 'next/navigation';
 import paths from '@/config/constants/paths';
 import Link from 'next/link';
 import { deleteCookie, getCookies } from 'cookies-next';
+import {
+  Navbar,
+  NavbarBrand,
+  NavbarContent,
+  NavbarItem,
+  Button,
+  NavbarMenuToggle,
+  NavbarMenu,
+  NavbarMenuItem,
+  Avatar,
+  Dropdown,
+  DropdownItem,
+  DropdownMenu,
+  DropdownTrigger,
+} from '@heroui/react';
 
 function AdminNavBar() {
   const router = useRouter();
-  const pathname = usePathname(); // Works in client-side components
+  const pathname = usePathname();
   const [activePath, setActivePath] = useState('');
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   const logout = async () => {
     const allCookies = getCookies();
@@ -26,57 +42,84 @@ function AdminNavBar() {
   }, [pathname]);
 
   return (
-    <nav className="bg-white text-primary flex justify-between items-center shadow-md px-10">
-      <div className="flex items-center justify-between">
-        <ul className="flex gap-10">
-          {[
-            { name: 'Overview', path: paths.adminDashboard },
-            { name: 'Posts', path: paths.adminPosts },
-            { name: 'Projects', path: paths.adminWorks },
-          ].map((item) => (
-            <Link key={item.path} href={item.path}>
-              <li
-                className={`py-5 cursor-pointer border-b-8 transition-all duration-300 ${
-                  activePath === item.path
-                    ? 'text-accent-primary border-accent-primary font-semibold'
-                    : 'border-transparent hover:text-accent-primary hover:border-accent-primary'
-                }`}
-              >
-                {item.name}
-              </li>
-            </Link>
-          ))}
-        </ul>
-      </div>
+    <Navbar onMenuOpenChange={setIsMenuOpen} className="bg-white text-primary shadow-sm px-10">
+      <NavbarBrand>
+        <p className="font-bold text-inherit">Admin Panel</p>
+      </NavbarBrand>
 
-      <div
-        className="flex items-center gap-3 cursor-pointer hover:text-accent-primary transition-colors"
-        onClick={logout}
-      >
-        <p className="flex items-center gap-1">Logout</p>
-      </div>
-      {/* <div className="hidden md:flex gap-5 items-center">
+      <NavbarContent className="hidden sm:flex gap-10" justify="center">
         {[
-          { icon: 'fas fa-search', color: 'text-primary' },
-          { icon: 'far fa-comments', color: 'text-primary' },
-          { icon: 'far fa-bell', color: 'text-primary' },
-        ].map((item, index) => (
-          <div key={index} className="flex items-center gap-2">
-            <i
-              className={`${item.icon} cursor-pointer ${item.color} hover:text-accent-primary transition-colors`}
-            />
-            {index < 2 && <span className="text-gray-400">|</span>}
-          </div>
+          { name: 'Overview', path: paths.adminDashboard },
+          { name: 'Posts', path: paths.adminPosts },
+          { name: 'Projects', path: paths.adminWorks },
+        ].map((item) => (
+          <NavbarItem key={item.path} isActive={activePath === item.path}>
+            <Link
+              href={item.path}
+              className={`py-5 cursor-pointer border-b-8 transition-all duration-300 ${activePath === item.path ? 'text-accent-primary border-accent-primary font-semibold' : 'border-transparent hover:text-accent-primary hover:border-accent-primary'}`}
+            >
+              {item.name}
+            </Link>
+          </NavbarItem>
         ))}
-        <span className="text-gray-400">|</span>
-        <div className="flex items-center gap-3 cursor-pointer hover:text-accent-primary transition-colors">
-          <i className="far fa-user-circle text-xl"></i>
-          <p className="flex items-center gap-1" onClick={logout}>
-            Vallendito <i className="fas fa-chevron-down"></i>
-          </p>
-        </div>
-      </div> */}
-    </nav>
+      </NavbarContent>
+
+      <NavbarContent as="div" justify="end">
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" onClick={logout} color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? 'Close menu' : 'Open menu'}
+          className="sm:hidden"
+        />
+      </NavbarContent>
+
+      <NavbarMenu>
+        {[
+          { name: 'Overview', path: paths.adminDashboard },
+          { name: 'Posts', path: paths.adminPosts },
+          { name: 'Projects', path: paths.adminWorks },
+          { name: 'Logout', action: logout },
+        ].map((item, index) => (
+          <NavbarMenuItem key={index}>
+            {item.path ? (
+              <Link href={item.path} className="w-full text-lg">
+                {item.name}
+              </Link>
+            ) : (
+              <button onClick={item.action} className="w-full text-lg text-red-500">
+                {item.name}
+              </button>
+            )}
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
+    </Navbar>
   );
 }
 
