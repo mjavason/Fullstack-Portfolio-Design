03a4d3e57a91e5c3bccb05276782bb59783d6cfa
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-04-18 14:58:18 +0100

    Add search input and SearchIcon component to AdminNavbar

--- frontend/src/components/admin/navigation/desktop.tsx
+++ frontend/src/components/admin/navigation/desktop.tsx
@@ -3,6 +3,8 @@
 import { usePathname } from 'next/navigation';
 import paths from '@/config/constants/paths';
 import Link from 'next/link';
+import { Input } from '@heroui/react';
+import SearchIcon from '@/components/admin/icons/search-icon';
 
 interface AdminNavbarProps {
   logout: () => void;
@@ -12,34 +14,43 @@ function AdminNavbar({ logout }: AdminNavbarProps) {
   const pathname = usePathname();
 
   return (
-    <ul className="hidden sm:flex gap-10 font-bold">
-      {[
-        { name: 'Overview', path: paths.adminDashboard },
-        { name: 'Posts', path: paths.adminPosts },
-        { name: 'Projects', path: paths.adminWorks },
-        { name: 'Logout', action: logout },
-      ].map((item, index) => (
-        <li key={index} className="transition-colors duration-300 cursor-pointer">
-          {item.path ? (
-            <Link
-              key={item.path}
-              href={item.path}
-              className={`text-2xl ${
-                pathname === item.path
-                  ? 'text-accent-primary font-semibold'
-                  : 'hover:text-accent-primary'
-              }`}
-            >
-              {item.name}
-            </Link>
-          ) : (
-            <button key={index} onClick={item.action} className="text-2xl">
-              {item.name}
-            </button>
-          )}
-        </li>
-      ))}
-    </ul>
+    <div className="w-full flex justify-between">
+      <Input
+        placeholder="Enter keyword to search"
+        startContent={<SearchIcon className="text-primary pointer-events-none" />}
+        type="text"
+        className="max-w-[75%] md:max-w-[35%]"
+        variant="bordered"
+      />
+      <ul className="hidden sm:flex gap-10 font-bold">
+        {[
+          { name: 'Overview', path: paths.adminDashboard },
+          { name: 'Posts', path: paths.adminPosts },
+          { name: 'Projects', path: paths.adminWorks },
+          { name: 'Logout', action: logout },
+        ].map((item, index) => (
+          <li key={index} className="transition-colors duration-300 cursor-pointer">
+            {item.path ? (
+              <Link
+                key={item.path}
+                href={item.path}
+                className={`text-2xl ${
+                  pathname === item.path
+                    ? 'text-accent-primary font-semibold'
+                    : 'hover:text-accent-primary'
+                }`}
+              >
+                {item.name}
+              </Link>
+            ) : (
+              <button key={index} onClick={item.action} className="text-2xl">
+                {item.name}
+              </button>
+            )}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
 
