04e91653094885e17f1f6d51af0d291ab7464ba1
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-04-19 19:13:59 +0100

    Update RecentPostCard styles and enhance summary display with additional content

--- frontend/src/components/sections/recent-posts/card.tsx
+++ frontend/src/components/sections/recent-posts/card.tsx
@@ -7,14 +7,14 @@ function RecentPostCard(props: { post: IPost }) {
 
   return (
     <Link href={paths.blogDetails(post.id)}>
-      <div className="flex flex-col cursor-pointer bg-white p-5 hover:border-1 h-full">
+      <div className="flex flex-col cursor-pointer bg-white p-5 hover:border-1 md:h-full">
         <h3 className="text-3xl font-bold my-5 flex-1 line-clamp-2 h-full pb-3">{post.title}</h3>
         <div className="my-3 text-lg">
           <span>{formatLongDate(post.createdAt, 'short')}</span>
           <span className="mx-5">|</span>
           <span>{post.categories.join(', ')}</span>
         </div>
-        <p className="flex-1 text-lg break-words line-clamp-2">{post.summary}</p>
+        <p className="flex-1 text-lg break-words h-full line-clamp-3">{post.summary}</p>
       </div>
     </Link>
   );
