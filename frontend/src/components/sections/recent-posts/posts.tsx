a4d0f030152eb436ef530f0e112eb4e373270859
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-04-19 18:22:25 +0100

    Refactor RecentPosts component to use RecentPostCard for rendering posts and update formatLongDate function to accept month format parameter

--- frontend/src/components/sections/recent-posts/posts.tsx
+++ frontend/src/components/sections/recent-posts/posts.tsx
@@ -1,27 +1,16 @@
 import { fetchPosts } from '@/actions/post/read';
-import paths from '@/config/constants/paths';
-import { formatLongDate } from '@/utils/date';
-import Link from 'next/link';
+import RecentPostCard from './card';
 
 async function RecentPosts() {
   const posts = await fetchPosts();
-  const renderedPosts = posts.map((post) => {
-    return (
-      <Link key={post.id} href={paths.blogDetails(post.id)}>
-        <div className="flex flex-col cursor-pointer bg-white p-5 hover:border-1">
-          <h3 className="text-3xl font-bold my-5 flex-1">{post.title}</h3>
-          <div className="my-3 text-lg">
-            <span>{formatLongDate(post.createdAt)}</span>
-            <span className="mx-5">|</span>
-            <span>{post.categories.join(', ')}</span>
-          </div>
-          <p className="flex-1 text-lg">{post.summary}</p>
-        </div>
-      </Link>
-    );
-  });
 
-  return <div className="grid grid-cols-1 lg:grid-cols-2 items-stretch gap-5">{renderedPosts}</div>;
+  return (
+    <div className="grid grid-cols-1 lg:grid-cols-2 items-stretch gap-5">
+      {posts.map((post) => {
+        return <RecentPostCard key={post.id} post={post} />;
+      })}
+    </div>
+  );
 }
 
 export default RecentPosts;
--- frontend/src/utils/date.ts
+++ frontend/src/utils/date.ts
@@ -3,13 +3,13 @@
  * @param {Date | string} inputDate - The date to format.
  * @returns {string} - Formatted date string.
  */
-export function formatLongDate(inputDate: Date | string) {
+export function formatLongDate(inputDate: Date | string, monthFormat: 'long' | 'short' = 'long') {
   const date = new Date(inputDate);
   if (isNaN(date.getTime())) throw new Error('Invalid date');
 
   return new Intl.DateTimeFormat('en-GB', {
     day: '2-digit',
-    month: 'long',
+    month: monthFormat,
     year: 'numeric',
   }).format(date);
 }
