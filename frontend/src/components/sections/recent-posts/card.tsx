288d26a7558427b957515fae153a5869b47b9f65
Orji Michael Chukwuebuka <orjimichael@4886@gmail.com>
2025-05-10 22:01:14 +0100

    Refactor RecentPostCard to accept index prop for animation delay; implement framer-motion for improved visual effects in recent posts section.

--- frontend/src/components/sections/recent-posts/card.tsx
+++ frontend/src/components/sections/recent-posts/card.tsx
@@ -1,13 +1,23 @@
+'use client';
+
+import { motion } from 'framer-motion';
 import paths from '@/config/constants/paths';
 import { formatLongDate } from '@/utils/date';
 import Link from 'next/link';
+import { time } from '@/config/motion';
 
-function RecentPostCard(props: { post: IPost }) {
-  const { post } = props;
+function RecentPostCard(props: { post: IPost; index: number }) {
+  const { post, index } = props;
 
   return (
     <Link href={paths.blogDetails(post.id)}>
-      <div className="flex flex-col cursor-pointer bg-white p-5 hover:border-1 md:h-full">
+      <motion.div
+        initial={{ scale: 0, opacity: 0 }}
+        whileInView={{ scale: 1, opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: time.fast, ease: 'easeOut', delay: index * time.fastest }}
+        className="flex flex-col cursor-pointer bg-white p-5 hover:border-1 md:h-full"
+      >
         <h3 className="text-3xl font-bold my-5 flex-1 md:first-letter:line-clamp-2 h-full pb-3">
           {post.title}
         </h3>
@@ -17,7 +27,7 @@ function RecentPostCard(props: { post: IPost }) {
           <span>{post.categories.join(', ')}</span>
         </div>
         <p className="flex-1 text-lg break-words h-full line-clamp-3">{post.summary}</p>
-      </div>
+      </motion.div>
     </Link>
   );
 }
--- frontend/src/components/sections/recent-posts/posts.tsx
+++ frontend/src/components/sections/recent-posts/posts.tsx
@@ -6,9 +6,9 @@ async function RecentPosts() {
 
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 items-stretch gap-5">
-      {posts.map((post) => {
-        return <RecentPostCard key={post.id} post={post} />;
-      })}
+      {posts.map((post, index) => (
+        <RecentPostCard key={post.id} post={post} index={index} />
+      ))}
     </div>
   );
 }
